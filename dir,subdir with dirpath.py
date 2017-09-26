b9ff438622cee27cf02910fb694ef73802736759
muneeb12342 <muneebwarraich12342@gmail.com>
2017-09-26 13:40:09 +0500

    v

--- dir,subdir with dirpath.py
+++ dir,subdir with dirpath.py
@@ -6,4 +6,4 @@ for root, dirnames, filenames in os.walk('C:/Users/JiN/Downloads'):
     for filename in fnmatch.filter(filenames, '*docx'):
         print(os.path.join(root, filename))
 #fn match is for file name pattern matching
-#os.path.joi is used for manipulate dir and file name
+#os.path.join is used for manipulate dir and file name

2c8cc06b95cd8d81eb197bbbb3c776d81bcefeab
muneeb12342 <muneebwarraich12342@gmail.com>
2017-09-26 13:32:51 +0500

    get files with dirpath

---
+++ dir,subdir with dirpath.py
@@ -0,0 +1,9 @@
+import fnmatch
+import os
+
+barlist = []
+for root, dirnames, filenames in os.walk('C:/Users/JiN/Downloads'):
+    for filename in fnmatch.filter(filenames, '*docx'):
+        print(os.path.join(root, filename))
+#fn match is for file name pattern matching
+#os.path.joi is used for manipulate dir and file name
